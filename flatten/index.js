edb6dfdd84068ca2e9ae4a612411c244478126ca
Ashish Kale <ashish.kale@uber.com>
2019-11-19 13:19:30 +0530

    add flatten

---
+++ flatten/index.js
@@ -0,0 +1,7 @@
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+
+module.exports = flatten;
